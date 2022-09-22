31fa6a8281e8d3cc0705b4a53af2d8f2eee3ecb8
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-09-22 13:50:55 +0900

    Initial Commit

---
+++ NavigationStackSample/NavigationStackSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackSampleApp.swift
+//  NavigationStackSample
+//
+//  Created by ミズキ on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
